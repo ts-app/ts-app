33d4a95e9931a86f18181a504e9b61899d16b115
kctang <kctang@big2.net>
2018-07-22 23:47:02 +0800

    Add validateEmail() and escapeRegex()

---
+++ packages/common/src/validateEmail.ts
@@ -0,0 +1,13 @@
+const isEmail = (email: string) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
+
+/**
+ * Validates an email address.
+ *
+ * Returns true if specified email is valid, false otherwise.
+ */
+export const validateEmail = (email: string): boolean => {
+  return !!(email && email.trim().length >= 3 && isEmail(email))
+}

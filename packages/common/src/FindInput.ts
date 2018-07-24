d244d3bbc626e9503f8f5a5e9c1eefbb8698ab9e
kctang <kctang@big2.net>
2018-07-24 23:31:38 +0800

    Add sort and project fields to FindInput

--- packages/common/src/FindInput.ts
+++ packages/common/src/FindInput.ts
@@ -2,7 +2,22 @@
  * Generic input parameters to perform a search with cursor based paging.
  */
 export interface FindInput {
+  /**
+   * Query string to search against. It is up to implementations to decide what to search based on
+   * the value of the search query.
+   */
   q?: string
+  /**
+   * Maximum number of results to return.
+   */
   limit?: number
   cursor?: string
+  /**
+   * Array of field names to sort against.
+   */
+  sort?: { field: string, asc: boolean }[]
+  /**
+   * A MongoDB specific parameter that represents fields to project.
+   */
+  project?: object
 }
